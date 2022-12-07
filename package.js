12555b1beb63452cf110ccaa64804d32c395548d
AMOS <108424978+Ramaunspoken@users.noreply.github.com>
2022-12-07 15:57:07 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 {
-  "name": "awesome-book-plain-javascript-with-objects",
+  "name": "awesome book",
   "version": "1.0.0",
   "description": "<a name=\"readme-top\"></a>",
-  "main": "Books.js",
+  "main": "main.js",
   "scripts": {
     "test": "echo \"Error: no test specified\" && exit 1"
   },
