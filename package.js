ceba665eeaa47404b817445837d6d8548b1fbb66
Patrick <70089310+Patfarmurs@users.noreply.github.com>
2022-12-07 05:50:47 -0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+  "name": "awesome-book-col",
+  "version": "1.0.0",
+  "description": "",
+  "main": "main.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/Ramaunspoken/Awesome-Book-col.git"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/Ramaunspoken/Awesome-Book-col/issues"
+  },
+  "homepage": "https://github.com/Ramaunspoken/Awesome-Book-col#readme"
+}
